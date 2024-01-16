ce544a44c0c4e9f61c45c5461c0b5b2475700287
senkorl <913265895@qq.com>
2024-01-16 14:34:22 +0800

    feat: gorm upd

--- db/gorm.go
+++ db/gorm.go
@@ -5,27 +5,11 @@ import (
 	"fmt"
 	"time"
 
+	"awesomeProject2/db/gorm_structs"
 	"github.com/golang-module/carbon/v2"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
-
-type UserEnt struct {
-	ID           uint           `json:"ID,omitempty"`
-	Name         string         `json:"name,omitempty"`
-	Email        *string        `json:"email,omitempty"`
-	Age          uint8          `json:"age,omitempty"`
-	Birthday     *time.Time     `json:"birthday,omitempty"`
-	MemberNumber sql.NullString `json:"memberNumber"`
-	ActivatedAt  sql.NullTime   `json:"activatedAt"`
-	CreatedAt    time.Time      `json:"createdAt"`
-	UpdatedAt    time.Time      `json:"updatedAt"`
-	DeletedAt    gorm.DeletedAt `gorm:"index" json:"deletedAt"`
-}
-
-func (u UserEnt) TableName() string {
-	return "user"
-}
 
 func Demo() {
 	DSN := "root:root@tcp(127.0.0.1:33060)/default?charset=utf8mb4&parseTime=True&loc=Local"
@@ -41,7 +25,7 @@ func Demo() {
 	}
 	email := "foo@qq.com"
 	birthday := time.Now()
-	insertUser := UserEnt{
+	insertUser := gorm_structs.UserEnt{
 		Name:         "xiaoming11",
 		Email:        &email,
 		Age:          23,
