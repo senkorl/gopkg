01cea6ff460534122d4a12e465d55c9de7875557
senkorl <913265895@qq.com>
2024-04-16 10:46:43 +0800

    feat: redis

--- db/redis/redis.go
+++ db/redis/redis.go
@@ -14,6 +14,15 @@ type Redis struct {
 	dbName    int
 }
 
+func New(addr, pwd string, dbName int) *Redis {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: pwd,    // 没有密码，默认值
+		DB:       dbName, // 默认DB 0
+	})
+	return &Redis{cli: rdb, addr: addr, pwd: pwd, dbName: dbName}
+}
+
 func (r *Redis) Get(key string) interface{} {
 	val, err := r.cli.Get(context.Background(), key).Result()
 	if err != nil {
@@ -26,13 +35,4 @@ func (r *Redis) Get(key string) interface{} {
 func (r *Redis) Set(key string, val interface{}, exp int) error {
 	_, err := r.cli.Set(context.Background(), key, val, time.Duration(exp)*time.Second).Result()
 	return err
-}
-
-func New(addr, pwd string, dbName int) *Redis {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     addr,
-		Password: pwd,    // 没有密码，默认值
-		DB:       dbName, // 默认DB 0
-	})
-	return &Redis{cli: rdb, addr: addr, pwd: pwd, dbName: dbName}
 }
